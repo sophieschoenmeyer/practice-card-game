1125eda0e9f98101f0066694100a445e4f817bc8
Carter Vande Moore <72159057+cvmoore@users.noreply.github.com>
2020-10-03 17:16:12 -0400

    Update Cards.java
    Added Constructor

--- Cards.java
+++ Cards.java
@@ -1,3 +1,11 @@
 public class Cards{
-    
+    int cardValue;
+    boolean cardColor;
+    String cardSuit;
+
+    public Cards(int cardValue, boolean cardColor, String cardSuit){
+        this.cardValue=cardValue;
+        this.cardColor=cardColor;
+        this.cardSuit=cardSuit;
+    }
 }
